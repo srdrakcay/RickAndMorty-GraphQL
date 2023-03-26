1febfe1fcb82af5f78be03ed297ed614c5917b40
srdrakcay <srdrakcay25@gmail.com>
2023-03-26 14:17:53 +0300

    Initial commit

---
+++ app/src/main/java/com/serdar/rickandmorty_graphql/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.serdar.rickandmorty_graphql
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
